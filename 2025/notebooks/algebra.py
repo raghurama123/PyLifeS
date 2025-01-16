53a01b7f29cba85327c6d51f091f5a000f02ab3f
Raghu <ramakrishnan@tifrh.res.in>
2025-01-16 22:19:09 +0530

    rr: pylifes25_01.ipynb added

---
+++ 2025/notebooks/algebra.py
@@ -0,0 +1,12 @@
+def square(x):
+    """
+    Returns the square of a number.
+    """
+    return x ** 2
+
+def add(a, b):
+    """
+    Returns the sum of two numbers.
+    """
+    return a + b
+
---
+++ 2025/notebooks/mymodule.py
@@ -0,0 +1,5 @@
+def greet():
+    print("Hello, Welcome!")
+
+def multiply(a, b):
+    return a * b
